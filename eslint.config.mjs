6cc58c454ef3cc97d9c8f86ac041823c92018d6d
ford burgess <fburg6824@gmail.com>
2025-06-09 13:10:57 -0500

    added storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -5,37 +8,32 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { enforceTypography } from './enforce-typography.mjs'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname
-      }
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'custom' : {
-        'enforce-typography' : enforceTypography
-      }
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      "@typescript-eslint/no-floating-promises": "error",
-      "@typescript-eslint/require-array-sort-compare": "error",
-      "custom/enforce-typography": "error"
-    },
+export default tseslint.config({ ignores: ['dist'] }, {
+  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname
+    }
   },
-  js.configs.recommended,
-  tseslint.configs.recommendedTypeChecked
-)
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    'custom' : {
+      'enforce-typography' : enforceTypography
+    }
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/require-array-sort-compare": "error",
+    "custom/enforce-typography": "error"
+  },
+}, js.configs.recommended, tseslint.configs.recommendedTypeChecked, storybook.configs["flat/recommended"]);
